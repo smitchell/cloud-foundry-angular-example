15b8d5593155f5a85c5cf882261ea5dbab40c15c
kc-mitchell <steve.mitchell@mobilecapitalgroup.com>
2019-04-12 07:21:12 -0500

    initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to spring-boot-angular-example!');
+    expect(page.getTitleText()).toEqual('Welcome to cloud-foundry-angular-example!');
   });
 
   afterEach(async () => {
